faef826076d1bc2599011b3ff999f2b3531d70d0
Andres Arevalo <9005829@alumnos.ufv.es>
2023-06-26 16:39:25 +0200

    Creada funcion pokemons que retorna una lista vacia de pokemons en la clase PokemonController

---
+++ pokemonAPI/src/main/java/dis/ufv/pokemonRestAPI/pokemonAPI/PokemonController.java
@@ -0,0 +1,13 @@
+package dis.ufv.pokemonRestAPI.pokemonAPI;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+@RestController
+public class PokemonController {
+    @GetMapping("/pokemons")
+    public  ArrayList<Pokemon> pokemons(){
+        ArrayList<Pokemon> listaPokemons = null;
+        return  listaPokemons;
+    }
+}
